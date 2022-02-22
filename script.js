c893c40f845366bbc7b230879030707fdf79034a
theopmw <theowrightdev@gmail.com>
2022-02-22 18:21:46 +0000

    Add logic to remove hidden class from modal on click event

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// Loop over open modal buttons and add click event listener
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+  });
